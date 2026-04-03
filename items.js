996c3347b0243bfa2cb0ae1ead50aaed053eed40
Davesalud <98496300+Davesalud@users.noreply.github.com>
2026-04-03 16:17:48 +0200

    Update items.js

--- items.js
+++ items.js
@@ -2,19 +2,19 @@ const ITEMS = {
     "fakard_01": { 
         id: "fakard_01", 
         name: "Ócska Fakard", 
-        type: "fegyver", // JAVÍTVA: kisbetűvel kell!
+        type: "fegyver", // JAVÍTVA: kisbetű!
         atk: 2, 
         def: 0, 
         rarity: "common", 
         img: "https://i.ibb.co/1YryWDNW/Copilot-20260403-152801.png" 
     },
-    "szakadtfelso_01": { // JAVÍTVA: a kulcs egyezzen az ID-vel!
+    "szakadtfelso_01": { 
         id: "szakadtfelso_01", 
         name: "Szakadt Felső", 
-        type: "páncél", // JAVÍTVA: kisbetűvel kell!
+        type: "páncél", // JAVÍTVA: kisbetű!
         atk: 0, 
         def: 2, 
-        rarity: "common", // JAVÍTVA: egy szakadt felső inkább common :)
+        rarity: "common", 
         img: "https://i.ibb.co/zw8sSP7/Copilot-20260403-153302.png" 
     }
 };
