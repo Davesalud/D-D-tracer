4317b66f6e05687e356086a4f7d57950c8ea8088
Davesalud <98496300+Davesalud@users.noreply.github.com>
2026-04-03 16:09:57 +0200

    Update items.js

--- items.js
+++ items.js
@@ -1,19 +1,19 @@
 const ITEMS = {
     "fakard_01": { 
         id: "fakard_01", 
-        name: "Fakard", 
+        name: "Ocska Fakard", 
         type: "Fegyver", 
-        atk: 3, 
+        atk: 2, 
         def: 0, 
         rarity: "common", 
         img: "https://i.ibb.co/1YryWDNW/Copilot-20260403-152801.png" 
     },
     "lovagpancel_01": { 
-        id: "lovagpancel_01", 
-        name: "Lovagi Páncél", 
+        id: "szakadtfelso_01", 
+        name: "Szakadt Felső", 
         type: "Páncél", 
         atk: 0, 
-        def: 12, 
+        def: 2, 
         rarity: "rare", 
         img: "https://i.ibb.co/zw8sSP7/Copilot-20260403-153302.png" 
     }
